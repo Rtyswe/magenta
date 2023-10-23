481bff109ec5a2b30ee7f2af6fb0c517ba0728db
rtyswe <konstantin.nikolaev.01@mail.ru>
2023-10-23 13:58:16 +0400

    add link between city and distance

--- src/main/java/com/example/magenta/model/Distance.java
+++ src/main/java/com/example/magenta/model/Distance.java
@@ -1,25 +1,37 @@
 package com.example.magenta.model;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
+@Getter
 public class Distance {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "from_city")
-    private String from;
-    @Column(name = "to_city")
-    private String to;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "from_id", nullable = false)
+    private City from;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "to_id", nullable = false)
+    private City to;
+
     private Double distance;
 }
--- src/main/java/com/example/magenta/service/XmlUploadService.java
+++ src/main/java/com/example/magenta/service/XmlUploadService.java
@@ -2,18 +2,24 @@ package com.example.magenta.service;
 
 import com.example.magenta.exception.EmptyFileException;
 import com.example.magenta.mapper.CityMapper;
-import com.example.magenta.mapper.DistanceMapper;
 import com.example.magenta.model.City;
 import com.example.magenta.model.Distance;
 import com.example.magenta.repo.CityRepository;
 import com.example.magenta.repo.DistanceRepository;
+import com.example.magenta.xml.CityData;
+import com.example.magenta.xml.DistanceData;
 import com.example.magenta.xml.UploadData;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -27,11 +33,30 @@ public class XmlUploadService {
                 (CollectionUtils.isEmpty(data.getCities()) && CollectionUtils.isEmpty(data.getDistances()))) {
             throw new EmptyFileException();
         }
+        saveCities(data.getCities());
+        saveDistances(data.getDistances());
+    }
 
-        List<City> cities = CollectionUtils.emptyIfNull(data.getCities()).stream().map(CityMapper::map).toList();
-        cityRepository.saveAll(cities);
+    private void saveCities(List<CityData> cities) {
+        Collection<City> toSave = CollectionUtils.emptyIfNull(cities).stream().map(CityMapper::map).toList();
+        cityRepository.saveAll(toSave);
+    }
 
-        List<Distance> distances = CollectionUtils.emptyIfNull(data.getDistances()).stream().map(DistanceMapper::map).toList();
-        distanceRepository.saveAll(distances);
+    private void saveDistances(List<DistanceData> distances) {
+        Map<String, City> cities = cityRepository.findAllByNames(
+                Stream.concat(distances.stream().map(DistanceData::getFrom), distances.stream().map(DistanceData::getTo))
+                    .collect(Collectors.toSet())).stream().collect(Collectors.toMap(City::getName, Function.identity()));
+
+        List<Distance> toSave = distances.stream().map(distanceData ->
+                        Distance.builder()
+                                .from(cities.getOrDefault(distanceData.getFrom(),
+                                        City.builder().name(distanceData.getFrom()).build()))
+                                .to(cities.getOrDefault(distanceData.getTo(),
+                                        City.builder().name(distanceData.getTo()).build()))
+                                .distance(distanceData.getDistanceValue())
+                                .build()
+                ).toList();
+
+        distanceRepository.saveAll(toSave);
     }
 }
